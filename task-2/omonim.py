8757f0a4c62089b37460cd7e05f51cc1801c8535
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-02-17 09:54:23 +0500

    fixed

--- task-2/omonim.py
+++ task-2/omonim.py
@@ -1,21 +1,23 @@
 import pdfkit
 import threading
 
+
 def convert_to_pdf(url, file_name, config):
     pdfkit.from_url(url, file_name, configuration=config)
 
-path_wkh = r'C:\Users\Mubina\Documents\python 5\Exam5\task-2\omonims'
 
-config  = pdfkit.configuration(wkhtmltopdf=path_wkh)
+path_wkh = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+
+config = pdfkit.configuration(wkhtmltopdf=path_wkh)
 
 threads = []
 
 for i in range(1, 11):
     url = f'https://tilshunos.com/omonims/?page={i}'
     pdf_filename = str(i) + '.pdf'
-    thread = threading.Thread(target=convert_to_pdf, args=(url,pdf_filename, config))
+    thread = threading.Thread(target=convert_to_pdf, args=(url, pdf_filename, config))
 
-    thread.append(thread)
+    threads.append(thread)
     thread.start()
 
 for thread in threads:
